d7953c82470b8bc15addcaa4089c6ba89c1d98e5
Osval28 <osvalricardo2806@gmail.com>
2026-04-22 19:03:18 -0500

    vaciar

--- UserService/Controllers/BusinessController.cs
+++ UserService/Controllers/BusinessController.cs
@@ -8,18 +8,6 @@ namespace UserService.Controllers
     [Route("api/[controller]")]
     public class BusinessController : ControllerBase
     {
-        private readonly BusinessService _businessService;
-
-        public BusinessController(BusinessService businessService)
-        {
-            _businessService = businessService;
-        }
-
-        [HttpPost("create/{adminUserId}")]
-        public IActionResult CreateBusiness(int adminUserId, BusinessDTO dto)
-        {
-            _businessService.CreateBusiness(dto, adminUserId);
-            return Created("", new { message = "Business created successfully" });
-        }
+        
     }
 }
